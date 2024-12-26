648319397512b945dfa721dec0ba84c11259d27c
5urafel <surafel5540f@gmail.com>
2024-12-26 07:51:40 +0000

    Hero section, Button and Shoe card Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Nav from "./components/Nav";
+import Hero from "./sections/Hero";
 
 function App() {
   return (
-    <>
+    <main className="relative">
       <Nav />
-    </>
+
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+    </main>
   );
 }
 
